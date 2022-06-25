d01e0fae26041b07a94ccbe0afb5de6f2c747910
unknown <Dominion Ogbaji>
2022-06-25 10:12:47 +0100

    added two new files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
+    //BEM NAMING CONVENTION
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="app_body">
+        <Sidebar/>
+        {/*chatbar*/}
+      </div>
     </div>
   );
 }
