bcde0fed42dacb7b93e93d3eac24928b519d82ab
Enoma Victor <vickjunior2002@gmail.com>
2022-06-26 15:58:14 +0100

    Install material-ui and material-ui-icon

--- src/chatbar/Chatbar.js
+++ src/chatbar/Chatbar.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+
 import './Chatbar.css'
 
 class Chatbar extends Component {
     render() {
         return (
             <div className='chatbar'>
-                
             </div>
         );
     }
